d877db624512b8cee0dc8748c8f57242674a12b8
Christian Bone <bone.christian.c@outlook.com>
2026-02-06 12:26:27 -0700

    Add more visual elements, prepare for dynamic content

--- Volt/0.1.0/modules/index.js
+++ Volt/0.1.0/modules/index.js
@@ -1,15 +1,25 @@
+import * as fs from './fs.js'
 import * as load from './load.js'
 import * as tabs from './tabs.js'
 import * as dashboard from './dashboard.js'
 import * as newproject from './newproject.js'
+const os = require('os');
+const path = require('path');
 
 function wait(ms) {return new Promise((resolve) => {setTimeout(resolve, ms)})}
 
 export const nodeforge = {
-    sessionVersion: "0.1.0"
+    sessionVersion: "0.1.0",
+    sandbox: path.join(os.homedir(), 'nvxstdo', 'volt'),
+    sandboxPlain: path.join('nvxstdo', 'volt')
 }
 
 async function main() {
+    try {
+        await fs.init();
+    } catch {
+        document.body.innerHTML = "Failed to create files, Volt has stopped execution for your safety.";
+    }
     load.init();
     const animationDelay = 1000;
     
--- Volt/0.1.0/modules/newproject.js
+++ Volt/0.1.0/modules/newproject.js
@@ -1,9 +1,67 @@
 import * as tabs from './tabs.js'
+import * as main from './index.js'
+const os = require('os');
+const path = require('path');
+const fs = require('fs');
+
+// Variables
+let templatesPath;
+let templates = [];
+
+// Labels
+const creationLabel = getEBD('newProject1-creationHeader');
+const editingLabel = getEBD('newProject1-editingHeader');
+
+// Button Sections
+const creationButtons = getEBD('newProject1-creationActions');
+const editingButtons = getEBD('newProject1-editingActions');
+
+// Editing Buttons
+const editingBack = getEBD('newProject1-editingBack');
+
+// Creation Buttons
+const manageTemplates = getEBD('newProject1-manageTemplates');
+const cancelBtn = getEBD('newProject1-cancel');
 
 function getEBD(id) {return document.getElementById(id)};
 
 export function init() {
-    const cancelBtn = getEBD('newProject1-cancel');
-
     cancelBtn.addEventListener('click', function() { tabs.goto("dashboard"); });
+    manageTemplates.addEventListener('click', toggleEditingMode);
+    editingBack.addEventListener('click', toggleEditingMode);
+    
+    
+    templatesPath = path.join(os.homedir(), main.nodeforge.sandboxPlain, 'templates');
+    console.log(`Template Directory: ${templatesPath}`);
+}
+
+let editMode = false;
+
+function toggleEditingMode() {
+    if (!editMode) {
+        creationButtons.classList.add('hidden');
+        editingButtons.classList.remove('hidden');
+        creationLabel.classList.add('hidden');
+        editingLabel.classList.remove('hidden');
+    } else {
+        creationButtons.classList.remove('hidden');
+        editingButtons.classList.add('hidden');
+        creationLabel.classList.remove('hidden');
+        editingLabel.classList.add('hidden');
+    }
+    editMode = !editMode;
+}
+
+function loadTemplates() {
+    const allFiles = fs.readdirSync(templatesPath);
+    allFiles.forEach(function(file) {
+        if (file.includes('.vtpl')) {
+            templates.push(file);
+        }
+    });
+}
+
+function loadTemplate(template, button) {
+    const templatePath = path.join(templatesPath, `${template}.vtpl`);
+    const templateMeta = JSON.parse(fs.readFileSync(templatePath));
 }
