ef2703fa6cc510c13f96d31e6f56964aaa9446fb
Christian Bone <bone.christian.c@outlook.com>
2026-02-08 08:47:09 -0700

    Fix more inconsistencies, work on template loading

--- Volt/0.1.0/modules/newproject.js
+++ Volt/0.1.0/modules/newproject.js
@@ -4,11 +4,15 @@ const os = require('os');
 const path = require('path');
 const fs = require('fs');
 
+// Helper Functions
+function getEBD(id) {return document.getElementById(id)};
+function pretty(str) {return str.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');}
+
 // Variables
 let templatesPath;
 let templates = [];
 
-// Labels
+// Headers
 const creationLabel = getEBD('newProject1-creationHeader');
 const editingLabel = getEBD('newProject1-editingHeader');
 
@@ -16,28 +20,35 @@ const editingLabel = getEBD('newProject1-editingHeader');
 const creationButtons = getEBD('newProject1-creationActions');
 const editingButtons = getEBD('newProject1-editingActions');
 
-// Editing Buttons
+// Buttons
+const creationManage = getEBD('newProject1-creationManage');
+const creationRefresh = getEBD('newProject1-creationRefresh');
+const creationCancel = getEBD('newProject1-creationCancel');
 const editingBack = getEBD('newProject1-editingBack');
+const editingRefresh = getEBD('newProject1-editingRefresh');
+const editingCreate = getEBD('newProject1-editingCreate');
 
-// Creation Buttons
-const manageTemplates = getEBD('newProject1-manageTemplates');
-const cancelBtn = getEBD('newProject1-cancel');
-
-function getEBD(id) {return document.getElementById(id)};
+// Misc
+const templateList = getEBD("newProject1-templateList");
+const templateView = getEBD("newProject1-templateView");
 
 export function init() {
-    cancelBtn.addEventListener('click', function() { tabs.goto("dashboard"); });
-    manageTemplates.addEventListener('click', toggleEditingMode);
-    editingBack.addEventListener('click', toggleEditingMode);
-    
-    
     templatesPath = path.join(os.homedir(), main.volt.sandboxPlain, 'templates');
     console.log(`Template Directory: ${templatesPath}`);
+    if (!fs.existsSync(templatesPath)) fs.mkdirSync(templatesPath);
+    
+    creationCancel.addEventListener('click', function() { tabs.goto("dashboard"); });
+    creationManage.addEventListener('click', toggleEditingMode);
+    editingBack.addEventListener('click', toggleEditingMode);
+    creationRefresh.addEventListener('click', populateTemplateList);
+    editingRefresh.addEventListener('click', populateTemplateList);
 }
 
 let editMode = false;
 
 function toggleEditingMode() {
+    templateView.classList.add('hidden');
+    templateView.innerHTML = "";
     if (!editMode) {
         creationButtons.classList.add('hidden');
         editingButtons.classList.remove('hidden');
@@ -50,18 +61,62 @@ function toggleEditingMode() {
         editingLabel.classList.add('hidden');
     }
     editMode = !editMode;
+    populateTemplateList();
 }
 
-function loadTemplates() {
+function populateTemplateList() {
+    templateList.innerHTML = "";
+    templates = [];
     const allFiles = fs.readdirSync(templatesPath);
     allFiles.forEach(function(file) {
         if (file.includes('.vtpl')) {
-            templates.push(file);
+            let meta;
+            try {
+                meta = JSON.parse(fs.readFileSync(path.join(templatesPath, file)));
+            } catch(err) {
+                console.error(`Failed to parse meta data for template: ${file}. Reason: ${err}`);
+                return;
+            }
+            templates.push({src: file, meta: meta});
         }
     });
+    if (templates.length) {
+        templates.forEach(function(template) {
+            const button = document.createElement('button');
+            button.id = `newProject1-${template.meta.id}-button`;
+            button.innerText = template.meta.name || template;
+            button.addEventListener('click', function() { loadTemplate(template, this) });
+            templateList.appendChild(button);
+        });
+    } else {
+        if (editMode) {
+            templateList.innerHTML = "No Templates Found. Press Create Template to make one.";
+        } else {
+            templateList.innerHTML = "No Templates Found. Press Manage Templates to make one.";
+        }
+    }
 }
 
+let currentStateValues = {};
+
+const properties = {
+    "directoryStructure": {type: "checkbox"}
+};
+
 function loadTemplate(template, button) {
-    const templatePath = path.join(templatesPath, `${template}.vtpl`);
-    const templateMeta = JSON.parse(fs.readFileSync(templatePath));
+    console.log("Loading Template");
+    template.meta.values.forEach(function(value) {
+        const input = document.createElement('input');
+        const label = document.createElement('label');
+        const inputType = properties[value.property].type;
+        const id = `newProject1-${template.meta.id}-${value.property}`;
+        console.log(id);
+        input.type = inputType;
+        if (inputType == "checkbox") {
+            input.checked = value.value;
+        } else {
+            input.value = value.value;
+        }
+        
+    });
 }
